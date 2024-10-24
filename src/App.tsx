60dc7d83fc1028ffeea9288d59835d189071c453
Kim-Hyunhee <tta_bi@naver.com>
2024-10-24 10:27:32 +0900

    jsx 문법

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,35 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+{
+  /* 
+  작성자: khh
+  작성일: 2024.**.**
+  내용: 기능에 대한 내용
+  */
+}
+
 function App() {
-  // let name = "리액트";
+  let name = "리액트";
+  /* 인라인 스타일 */
+  const style = {
+    backgroundColor: "black",
+    color: "white",
+    fontSize: "48px",
+    fontWeight: "bold",
+    padding: "20px",
+  };
 
-  // return (
-  //   <div className="container">
-  //     <h1 className="test">
-  //       Hello, {name === "리액트" ? <h1>YES</h1> : <h1>null</h1>}!!
-  //     </h1>
-  //     <p>반갑습니다.</p>
-  //   </div>
-  // );
-
-  const port = undefined;
-
-  return <div>{port || "3000"}</div>;
+  return (
+    <div className="container">
+      <h1 className="test">
+        Hello, {name === "리액트" ? <h1>YES</h1> : <h1>null</h1>}!!
+      </h1>
+      <p>반갑습니다.</p>
+      <br />
+      <input />
+    </div>
+  );
 }
 
 export default App;
