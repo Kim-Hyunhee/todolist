a180209328b71ead5505b388fcacf4231912029d
Kim-Hyunhee <tta_bi@naver.com>
2024-10-25 11:51:58 +0900

    modal

---
+++ src/TodoModal.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Modal } from "react-bootstrap";
+
+type Todo = {
+  id: number;
+  text: string;
+  isChecked: boolean;
+};
+
+const TodoModal: React.FC = () => {
+  return <div></div>;
+};
+
+export default TodoModal;
