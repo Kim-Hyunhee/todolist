644571b6d165f017c9d764441bb5e10d77f87bf3
Kim-Hyunhee <tta_bi@naver.com>
2024-10-25 09:56:52 +0900

    클래스형, 함수형 컴포넌트

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
-{
-  /* 
-  작성자: khh
-  작성일: 2024.**.**
-  내용: 기능에 대한 내용
-  */
-}
+import ClassCom from "./ClassCom";
+import FuncCom from "./FuncCom";
 
 function App() {
   let name = "리액트";
-  /* 인라인 스타일 */
-  const style = {
-    backgroundColor: "black",
-    color: "white",
-    fontSize: "48px",
-    fontWeight: "bold",
-    padding: "20px",
-  };
-
   return (
     <div className="container">
-      <h1 className="test">
-        Hello, {name === "리액트" ? <h1>YES</h1> : <h1>null</h1>}!!
-      </h1>
-      <p>반갑습니다.</p>
-      <br />
-      <input />
+      <ClassCom></ClassCom>
+      <FuncCom></FuncCom>
     </div>
   );
 }
