c6da3b24f06d1a31dc5acb73731a64085acb5c53
Kim-Hyunhee <tta_bi@naver.com>
2024-10-25 10:13:03 +0900

    구조 분해 할당

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -3,18 +3,25 @@ import React, { useState } from "react";
 const TodoList: React.FC = () => {
   const title: string = "오늘 할일";
 
-  const [todos] = useState(["공부하기", "잠자기", "미팅하기"]);
+  // 구조 분해 할당
+  const [todos, setTodos] = useState<string[]>([
+    "공부하기",
+    "잠자기",
+    "미팅하기",
+  ]);
 
   return (
     <div>
       <h1>{title}</h1>
       <p></p>
       <div className="container">
-        <ul>
-          <li>{todos[0]}</li>
-          <li>{todos[1]}</li>
-          <li>{todos[2]}</li>
-        </ul>
+        <div className="board">
+          <ul>
+            <li>{todos[0]}</li>
+            <li>{todos[1]}</li>
+            <li>{todos[2]}</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
