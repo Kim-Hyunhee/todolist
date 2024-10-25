1c1e738f77ed16184c8755c7b0adb1ac6c897427
Kim-Hyunhee <tta_bi@naver.com>
2024-10-25 10:55:32 +0900

    시계 추가하기

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import TodoList from "./Todolist";
+import Clock from "./Timer";
 
 function App() {
   let name = "리액트";
   return (
     <div className="container">
       <TodoList></TodoList>
+      <Clock></Clock>
     </div>
   );
 }
